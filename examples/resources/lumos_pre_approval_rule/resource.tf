88b962932dc69aba0263531b4d3694f6a2cc22a7
Ethan Houston <ethan.houston@gmail.com>
2024-06-30 22:56:42 -0500

    Add examples for permission and pre approval

--- examples/resources/lumos_pre_approval_rule/resource.tf
+++ examples/resources/lumos_pre_approval_rule/resource.tf
@@ -1,4 +1,11 @@
-resource "lumos_pre_approval_rule" "my_preapprovalrule" {
-  app_id        = "...my_app_id..."
-  justification = "...my_justification..."
+resource "lumos_pre_approval_rule" "eng_dev" {
+  app_id        = local.aws_app_id
+  justification = "All developers should be pre-approved for dev access."
+
+  preapproved_permissions = [
+    { id = local.dev_permission_id }
+  ]
+  preapproved_groups = [
+    { id = local.dev_group_id }
+  ]
 }
