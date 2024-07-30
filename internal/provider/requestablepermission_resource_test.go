1028b96f7c72c03c936c3df82889a3b8f2a4e267
Niamh Durfee <niamh@lumos.com>
2024-07-30 14:39:45 -0700

    Add tests around TBA

--- internal/provider/requestablepermission_resource_test.go
+++ internal/provider/requestablepermission_resource_test.go
@@ -14,12 +14,11 @@ func TestAccPermissionResource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
-			// Read testing
 			{
 				Config: providerConfig() + fmt.Sprintf(`
-				resource "lumos_requestable_permission" "test_permission_with_overrides" {
+				resource "lumos_requestable_permission" "test_permission_with_overrides_1" {
 					app_id      = "%s"
-					label       = "Permission with Config Overrides"
+					label       = "Permission with Config Overrides 1"
 					request_config = {
 						allowed_groups_override = true
 						allowed_groups = {
@@ -42,8 +41,56 @@ func TestAccPermissionResource(t *testing.T) {
 					}
 				}`, os.Getenv("APP_ID"), os.Getenv("GROUP_ID"), os.Getenv("USER_ID")),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("lumos_requestable_permission.test_permission_with_overrides", "label", "Permission with Config Overrides"),
-					resource.TestCheckResourceAttr("lumos_requestable_permission.test_permission_with_overrides", "type", "NATIVE"),
+					resource.TestCheckResourceAttr("lumos_requestable_permission.test_permission_with_overrides_1", "label", "Permission with Config Overrides 1"),
+					resource.TestCheckResourceAttr("lumos_requestable_permission.test_permission_with_overrides_1", "type", "NATIVE"),
+				),
+			},
+			{
+				Config: providerConfig() + fmt.Sprintf(`
+				resource "lumos_requestable_permission" "test_permission_with_overrides_2" {
+					app_id      = "%s"
+					label       = "Permission with Config Overrides 2"
+					request_config = {
+						allowed_groups_override = true
+						allowed_groups = {
+							type = "ALL_GROUPS"
+						}
+						appstore_visibility = "HIDDEN"
+						request_approval_config = {
+							request_approval_config_override = true
+							approvers = {
+								users  = [{ id = "%s" }]
+								groups = []
+							}
+						}
+					}
+				}`, os.Getenv("APP_ID"), os.Getenv("USER_ID")),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("lumos_requestable_permission.test_permission_with_overrides_2", "label", "Permission with Config Overrides 2"),
+					resource.TestCheckResourceAttr("lumos_requestable_permission.test_permission_with_overrides_2", "type", "NATIVE"),
+				),
+			},
+			{
+				Config: providerConfig() + fmt.Sprintf(`
+				resource "lumos_requestable_permission" "test_permission_with_overrides_3" {
+					app_id      = "%s"
+					label       = "Permission with Config Overrides 3"
+					request_config = {
+						allowed_groups_override = true
+						allowed_groups = {
+							groups = []
+							type = "ALL_GROUPS"
+						}
+						appstore_visibility = "HIDDEN"
+						request_approval_config = {
+							request_approval_config_override = true
+							manager_approval = "NONE"
+						}
+					}
+				}`, os.Getenv("APP_ID")),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("lumos_requestable_permission.test_permission_with_overrides_3", "label", "Permission with Config Overrides 3"),
+					resource.TestCheckResourceAttr("lumos_requestable_permission.test_permission_with_overrides_3", "type", "NATIVE"),
 				),
 			},
 		},
@@ -109,6 +156,77 @@ func TestAccPermissionResource(t *testing.T) {
 			}
 
 			// some simple example with string matching
+			if strings.Contains(err.Error(), "Invalid time based access") {
+				return nil
+			}
+
+			// return original error if no match
+			return err
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig() + fmt.Sprintf(`
+				resource "lumos_requestable_permission" "test_manager_approval" {
+					app_id      = "%s"
+					label       = "Permission Label"
+					request_config = {
+						request_fulfillment_config = {
+							time_based_access_override = true
+							time_based_access = ["unlimited"]
+						}
+					}
+				}`, os.Getenv("APP_ID")),
+			},
+			{
+				Config: providerConfig() + fmt.Sprintf(`
+				resource "lumos_requestable_permission" "test_manager_approval" {
+					app_id      = "%s"
+					label       = "Permission Label"
+					request_config = {
+						request_fulfillment_config = {
+							time_based_access_override = true
+							time_based_access = ["2 Hours"]
+						}
+					}
+				}`, os.Getenv("APP_ID")),
+			},
+			{
+				Config: providerConfig() + fmt.Sprintf(`
+				resource "lumos_requestable_permission" "test_manager_approval" {
+					app_id      = "%s"
+					label       = "Permission Label"
+					request_config = {
+						request_fulfillment_config = {
+							time_based_access_override = true
+							time_based_access = []
+						}
+					}
+				}`, os.Getenv("APP_ID")),
+			},
+			{
+				Config: providerConfig() + fmt.Sprintf(`
+				resource "lumos_requestable_permission" "test_manager_approval" {
+					app_id      = "%s"
+					label       = "Permission Label"
+					request_config = {
+						request_fulfillment_config = {
+							time_based_access_override = false
+							time_based_access = ["Unlimited"]
+						}
+					}
+				}`, os.Getenv("APP_ID")),
+			},
+		},
+	})
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		ErrorCheck: func(err error) error {
+			if err == nil {
+				return errors.New("expected error but got none")
+			}
+
+			// some simple example with string matching
 			if strings.Contains(err.Error(), "Invalid allowed groups") {
 				return nil
 			}
@@ -119,15 +237,15 @@ func TestAccPermissionResource(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: providerConfig() + fmt.Sprintf(`
-				resource "lumos_requestable_permission" "test_allowed_groups" {
+				resource "lumos_requestable_permission" "test_allowed_groups_override" {
 					app_id      = "%s"
-					label       = "Permission Label"
+					label       = "Invalid allowed groups permission"
 					request_config = {
 						allowed_groups = {
-							groups = [{ id = "123" }]
+							groups = [{ id = "%s" }]
 						}
 					}
-				}`, os.Getenv("APP_ID")),
+				}`, os.Getenv("APP_ID"), os.Getenv("GROUP_ID")),
 			},
 		},
 	})
