9b0d27a9b989613f65cbfb7dd4b4c782a632fc5e
Niamh Durfee <niamh@lumos.com>
2024-07-31 12:17:10 -0700

    More tests

--- internal/provider/app_resource_test.go
+++ internal/provider/app_resource_test.go
@@ -17,11 +17,15 @@ func TestAccAppResource(t *testing.T) {
 					name        = "Terraform Testing - Create app"
 					description = "Terraform Testing Description"
 					category    = "Developers"
+					logo_url    = "https://www.example.com/logo.png"
+					website_url = "https://www.example.com"
 				}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("lumos_app.test_app", "user_friendly_label", "Terraform Testing - Create app"),
 					resource.TestCheckResourceAttr("lumos_app.test_app", "instance_id", "custom_app_import"),
 					resource.TestCheckResourceAttr("lumos_app.test_app", "status", "APPROVED"),
+					resource.TestCheckResourceAttr("lumos_app.test_app", "logo_url", "https://www.example.com/logo.png"),
+					resource.TestCheckResourceAttr("lumos_app.test_app", "website_url", "https://www.example.com"),
 				),
 			},
 			// Update testing
@@ -31,11 +35,15 @@ func TestAccAppResource(t *testing.T) {
 					name        = "Terraform Testing - Update app"
 					description = "New Description"
 					category    = "Communication"
+					logo_url    = "https://www.example1.com/logo.png"
+					website_url = "https://www.example1.com"
 				}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("lumos_app.test_app", "user_friendly_label", "Terraform Testing - Update app"),
 					resource.TestCheckResourceAttr("lumos_app.test_app", "instance_id", "custom_app_import"),
 					resource.TestCheckResourceAttr("lumos_app.test_app", "status", "APPROVED"),
+					resource.TestCheckResourceAttr("lumos_app.test_app", "logo_url", "https://www.example1.com/logo.png"),
+					resource.TestCheckResourceAttr("lumos_app.test_app", "website_url", "https://www.example1.com"),
 				),
 			},
 		},
